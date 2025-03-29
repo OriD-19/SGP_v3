334d9e6b395c4946aec6dca6cb7cc59523f3eb9a
Fer Fuentes <fernandocastillo.8@hotmail.com>
2025-03-29 10:01:29 -0600

    Finished seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Priority;
+use App\Models\Role;
+use App\Models\Status;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,5 +22,58 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        Role::factory()->create([
+            'role' => 'scrum_master',
+        ]);
+
+        Role::factory()->create([
+            'role' => 'product_owner',
+        ]);
+
+        Role::factory()->create([
+            'role' => 'team_member',
+        ]);
+
+        Role::factory()->create([
+            'role' => 'admin',
+        ]);
+
+        Priority::factory()->create([
+            'priority' => 'low',
+        ]);
+
+        Priority::factory()->create([
+            'priority' => 'medium',
+        ]);
+
+        Priority::factory()->create([
+            'priority' => 'high',
+        ]);
+
+        Status::factory()->create([
+            'status' => 'backlog',
+        ]);
+
+        Status::factory()->create([
+            'status' => 'in_progress',
+        ]);
+
+        Status::factory()->create([
+            'status' => 'done',
+        ]);
+
+        Status::factory()->create([
+            'status' => 'in_review',
+        ]);
+
+        Status::factory()->create([
+            'status' => 'in_testing',
+        ]);
+
+        Status::factory()->create([
+            'status' => 'to_do',
+        ]);
+
     }
 }
