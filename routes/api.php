61586c8e7840babbe7e78d21c95a03221bcbc0f0
FernandaRubio74 <20235795@esen.edu.sv>
2025-03-31 00:45:03 -0600

    Auth tests - merge con Maria Rubio

--- routes/api.php
+++ routes/api.php
@@ -1,23 +1,48 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\OrganizationController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\SprintController;
 use App\Http\Controllers\StatusController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\UserStoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'v1'], function () {
-    Route::apiResource('organizations', OrganizationController::class);
+Route::group(['prefix' => 'SGP/v1'], function () {
+    // Rutas públicas
+    Route::post('register', [UserController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
 
-    Route::apiResource('organizations.projects', ProjectController::class);
+    // Rutas protegidas **
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('organizations', function () {
+            return response()->json(['message' => 'Organizations accessed successfully'], 200);
+        });
 
-    Route::apiResource('organizations.projects.user_stories', UserStoryController::class);
+        Route::get('projects', function () {
+            return response()->json(['message' => 'Projects accessed successfully'], 200);
+        });
 
-    Route::apiResource('organizations.projects.user_stories.tasks', TaskController::class);
+        Route::get('sprints', function () {
+            return response()->json(['message' => 'Sprints accessed successfully'], 200);
+        });
 
-    Route::apiResource('organizations.projects.sprints', SprintController::class);
+        Route::get('user-stories', function () {
+            return response()->json(['message' => 'User stories accessed successfully'], 200);
+        });
+
+        Route::get('tasks', function () {
+            return response()->json(['message' => 'Tasks accessed successfully'], 200);
+        });
+
+        Route::apiResource('organizations', OrganizationController::class);
+        Route::apiResource('organizations.projects', ProjectController::class);
+        Route::apiResource('organizations.projects.user_stories', UserStoryController::class);
+        Route::apiResource('organizations.projects.user_stories.tasks', TaskController::class);
+        Route::apiResource('organizations.projects.sprints', SprintController::class);
+    });
 });
